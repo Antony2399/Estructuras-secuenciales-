ec50fe0caf8df1c8c995fbf1d1665cbcf56cabca
Antony2399 <113643297+Antony2399@users.noreply.github.com>
2022-11-10 18:46:03 -0500

    Add files via upload

---
+++ tarea 2/script.js
@@ -0,0 +1,10 @@
+// valor de 1 pulgada = 0.0254 m
+
+let m;
+let pul = 0.0254 ;
+let conv; 
+
+m =Number(prompt('ingrese metros'))
+conv = m / pul;
+
+alert(`${m} Metros de tela son, ${conv} pulgas de tela`)
